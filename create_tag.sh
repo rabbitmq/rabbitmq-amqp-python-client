66af2b12963f42a558e909da2d9109742919a6da
Gabriele Santomaggio <G.santomaggio@gmail.com>
2025-05-07 16:05:51 +0200

    remove documentation duplication [skip ci]
    documentation is here https://www.rabbitmq.com/client-libraries/amqp-client-libraries
    
    Signed-off-by: Gabriele Santomaggio <G.santomaggio@gmail.com>

---
+++ create_tag.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+version=$1
+# Regex pattern for the valid strings
+regex="^v([0-9]+)\.([0-9]+)\.([0-9]+)(-(alpha|beta|rc)\.([0-9]+))?$"
+
+if [[ $version =~ $regex ]]; then
+    echo "Creating tag with: " $1
+    git tag -a -s -u $2 -m "rabbitmq-amqp-python-client $1" $1 && git push && git push --tags
+    echo "Tag created: " $1
+else
+    echo "Invalid version" $1
+fi
